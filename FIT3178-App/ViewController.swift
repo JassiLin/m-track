6a89583b1b0e18a560afd3f90082f03bd86240d2
Jingmei Lin <jlin0027@student.monash.edu>
2020-05-12 00:52:22 +1000

    Initial Commit

---
+++ FIT3178-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FIT3178-App
+//
+//  Created by Jingmei Lin on 12/5/20.
+//  Copyright © 2020 Jingmei Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
