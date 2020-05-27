1b06c99edb07cf75b92a45028cbbeafbfde933c0
Jingmei Lin <jlin0027@student.monash.edu>
2020-05-28 02:04:40 +1000

    Improved login interface

---
+++ FIT3178-App/Extensions/UIColor+Additions.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Additions.swift
+//  FIT3178-App
+//
+//  Created by Jingmei Lin on 21/5/20.
+//  Copyright © 2020 Jingmei Lin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+  
+    static var primary: UIColor {
+        return UIColor(red: 1 / 255, green: 93 / 255, blue: 48 / 255, alpha: 1)
+    }
+
+    static var incomingMessage: UIColor {
+        return UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1)
+    }
+    
+    static var btnPink: UIColor {
+        return UIColor(red: 216 / 255, green: 174 / 255, blue: 174 / 255, alpha: 1)
+    }
+  
+}
--- FIT3178-App/ViewControllers/SignInViewController.swift
+++ FIT3178-App/ViewControllers/SignInViewController.swift
@@ -9,19 +9,102 @@
 import UIKit
 import FirebaseAuth
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    var txtUser:UITextField!
+    var txtPwd:UITextField!
+    
+    var loginBtn: UIButton!
+    var offsetLeftHand:CGFloat = 60
+    
+    var imgLeftHand:UIImageView!
+    var imgRightHand:UIImageView!
+    
+    var imgLeftHandGone:UIImageView!
+    var imgRightHandGone:UIImageView!
+    
+    var showType:LoginShowType = LoginShowType.NONE
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // get screen size
+        let mainSize = UIScreen.main.bounds.size
+        
+        let imgLogin =  UIImageView(frame:CGRect(x: mainSize.width/2-211/2, y: 100, width: 211, height: 109))
+        imgLogin.image = UIImage(named:"owl-login")
+        imgLogin.layer.masksToBounds = true
+        self.view.addSubview(imgLogin)
+        
+        let rectLeftHand = CGRect(x: 61 - offsetLeftHand, y: 90, width: 40, height: 65)
+        imgLeftHand = UIImageView(frame:rectLeftHand)
+        imgLeftHand.image = UIImage(named:"owl-login-arm-left")
+        imgLogin.addSubview(imgLeftHand)
+        
+        let rectRightHand = CGRect(x: imgLogin.frame.size.width / 2 + 60, y: 90, width: 40, height: 65)
+        imgRightHand = UIImageView(frame:rectRightHand)
+        imgRightHand.image = UIImage(named:"owl-login-arm-right")
+        imgLogin.addSubview(imgRightHand)
+        
+        // subview
+        let vLogin =  UIView(frame:CGRect(x: 15, y: 200, width: mainSize.width - 30, height: 230))
+        vLogin.layer.borderWidth = 0.5
+        vLogin.layer.borderColor = UIColor.lightGray.cgColor
+        vLogin.backgroundColor = UIColor.white
+        self.view.addSubview(vLogin)
+        
+        let rectLeftHandGone = CGRect(x: mainSize.width / 2 - 100,
+                                      y: vLogin.frame.origin.y - 22, width: 40, height: 40)
+        imgLeftHandGone = UIImageView(frame:rectLeftHandGone)
+        imgLeftHandGone.image = UIImage(named:"icon_hand")
+        self.view.addSubview(imgLeftHandGone)
+        
+        let rectRightHandGone = CGRect(x: mainSize.width / 2 + 62,
+                                       y: vLogin.frame.origin.y - 22, width: 40, height: 40)
+        imgRightHandGone = UIImageView(frame:rectRightHandGone)
+        imgRightHandGone.image = UIImage(named:"icon_hand")
+        self.view.addSubview(imgRightHandGone)
+        
+        txtUser = UITextField(frame:CGRect(x: 30, y: 30, width: vLogin.frame.size.width - 60, height: 44))
+        txtUser.delegate = self
+        txtUser.layer.cornerRadius = 5
+        txtUser.layer.borderColor = UIColor.lightGray.cgColor
+        txtUser.layer.borderWidth = 0.5
+        txtUser.leftView = UIView(frame:CGRect(x: 0, y: 0, width: 44, height: 44))
+        txtUser.leftViewMode = UITextField.ViewMode.always
+        
+        let imgUser = UIImageView(frame:CGRect(x: 11, y: 11, width: 22, height: 22))
+        imgUser.image = UIImage(named:"iconfont-user")
+        txtUser.leftView!.addSubview(imgUser)
+        txtUser.autocapitalizationType = .none
+        vLogin.addSubview(txtUser)
+        
+        txtPwd = UITextField(frame:CGRect(x: 30, y: 90, width: vLogin.frame.size.width - 60, height: 44))
+        txtPwd.delegate = self
+        txtPwd.layer.cornerRadius = 5
+        txtPwd.layer.borderColor = UIColor.lightGray.cgColor
+        txtPwd.layer.borderWidth = 0.5
+        txtPwd.isSecureTextEntry = true
+        txtPwd.leftView = UIView(frame:CGRect(x: 0, y: 0, width: 44, height: 44))
+        txtPwd.leftViewMode = UITextField.ViewMode.always
+
+        let imgPwd = UIImageView(frame:CGRect(x: 11, y: 11, width: 22, height: 22))
+        imgPwd.image = UIImage(named:"iconfont-password")
+        txtPwd.leftView!.addSubview(imgPwd)
+        vLogin.addSubview(txtPwd)
+        
+        loginBtn = UIButton(frame: CGRect(x: 30, y: 160, width: vLogin.frame.size.width - 60, height: 44))
+        loginBtn.backgroundColor = UIColor.btnPink
+        loginBtn.setTitle("CONTINUE", for: .normal)
+        loginBtn.addTarget(self, action: #selector(loginAction), for: .touchUpInside)
+        vLogin.addSubview(loginBtn)
     }
     
-    @IBAction func signIn(_ sender: Any) {
-        Auth.auth().signIn(withEmail: emailTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+    @objc func loginAction(){
+        Auth.auth().signIn(withEmail: txtUser.text!, password: txtPwd.text!, completion: { (user, error) in
             if error == nil {
                 let vc = self.storyboard?.instantiateViewController(withIdentifier: "TabBar")
                 vc?.modalPresentationStyle = .fullScreen
@@ -33,6 +116,64 @@ class SignInViewController: UIViewController {
         })
     }
     
+    
+    func textFieldDidBeginEditing(_ textField:UITextField) {
+        if textField.isEqual(txtUser) {
+            if (showType != LoginShowType.PASS)
+            {
+                showType = LoginShowType.USER
+                return
+            }
+            showType = LoginShowType.USER
+             
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                self.imgLeftHand.frame = CGRect(
+                    x: self.imgLeftHand.frame.origin.x - self.offsetLeftHand,
+                    y: self.imgLeftHand.frame.origin.y + 30,
+                    width: self.imgLeftHand.frame.size.width, height: self.imgLeftHand.frame.size.height)
+                self.imgRightHand.frame = CGRect(
+                    x: self.imgRightHand.frame.origin.x + 48,
+                    y: self.imgRightHand.frame.origin.y + 30,
+                    width: self.imgRightHand.frame.size.width, height: self.imgRightHand.frame.size.height)
+                self.imgLeftHandGone.frame = CGRect(
+                    x: self.imgLeftHandGone.frame.origin.x - 70,
+                    y: self.imgLeftHandGone.frame.origin.y, width: 40, height: 40)
+                self.imgRightHandGone.frame = CGRect(
+                    x: self.imgRightHandGone.frame.origin.x + 30,
+                    y: self.imgRightHandGone.frame.origin.y, width: 40, height: 40)
+            })
+        }
+        
+        if textField.isEqual(txtPwd){
+            if (showType == LoginShowType.PASS)
+            {
+                showType = LoginShowType.PASS
+                return
+            }
+            showType = LoginShowType.PASS
+             
+            UIView.animate(withDuration: 0.5, animations: { () -> Void in
+                self.imgLeftHand.frame = CGRect(
+                    x: self.imgLeftHand.frame.origin.x + self.offsetLeftHand,
+                    y: self.imgLeftHand.frame.origin.y - 30,
+                    width: self.imgLeftHand.frame.size.width, height: self.imgLeftHand.frame.size.height)
+                self.imgRightHand.frame = CGRect(
+                    x: self.imgRightHand.frame.origin.x - 48,
+                    y: self.imgRightHand.frame.origin.y - 30,
+                    width: self.imgRightHand.frame.size.width, height: self.imgRightHand.frame.size.height)
+                self.imgLeftHandGone.frame = CGRect(
+                    x: self.imgLeftHandGone.frame.origin.x + 70,
+                    y: self.imgLeftHandGone.frame.origin.y, width: 0, height: 0)
+                self.imgRightHandGone.frame = CGRect(
+                    x: self.imgRightHandGone.frame.origin.x - 30,
+                    y: self.imgRightHandGone.frame.origin.y, width: 0, height: 0)
+            })
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
     /*
     // MARK: - Navigation
 
@@ -42,5 +183,10 @@ class SignInViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    enum LoginShowType {
+        case NONE
+        case USER
+        case PASS
+    }
 }
